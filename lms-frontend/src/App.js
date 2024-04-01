48050f022f5d194535121c25dc0472332823ca1f
Asim Attar <asimattar313@gmail.com>
2024-04-01 14:46:04 +0530

    Initial Commit

--- lms-frontend/src/App.js
+++ lms-frontend/src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import './Components/About.js'
 import LoginSignup from './Components/LoginSignup/LoginSignup';
-import { BrowserRouter as Router, Routes,Route,Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes,Route,Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div >
-      <Routes>
-        <Route path='/about'>
-        </Route>
-      </Routes>
-        <LoginSignup/>
-    </div>
+      
+       <LoginSignup/>
+   </div>
+    
   );
 }
 
--- lms-frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ lms-frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,44 +1,124 @@
-import React, { useState } from 'react'
-import './LoginSignup.css'
-
-import user_icon from '../Assests/person.png'
-import email_icon from '../Assests/email.png'
-import password_icon from '../Assests/password.png'
-
+import React, { useState, useEffect } from 'react';
+import './LoginSignup.css';
 
 const LoginSignup = () => {
+    const [action, setAction] = useState("Login");
 
-    const [action,setAction]=useState("Login")
+    const myMenuFunction = () => {
+        var i = document.getElementById("navMenu");
 
-  return (
-    <div className='container'>
-      <div className="header">
-        <div className="text">{action}</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-        {action==="Login"?<div></div>:<div className="input">
-        <img src={user_icon} alt="" />
-        <input type="text" placeholder='Name'/>
-      </div>}
-      
-      <div className="input">
-        <img src={email_icon} alt="" />
-        <input type="email" placeholder='Email'/>
-      </div>
-      <div className="input">
-        <img src={password_icon} alt="" />
-        <input type="password" placeholder='Password'/>
-      </div>
-      </div>
-      {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot Password? <span>Click Here!</span></div>}
-      
-      <div className="submit-container">
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-      </div>
-    </div>
-  )
-}
+        if (i.className === "nav-menu") {
+            i.className += " responsive";
+        } else {
+            i.className = "nav-menu";
+        }
+    }
 
-export default LoginSignup
+    const login = () => {
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        x.style.left = "4px";
+        y.style.right = "-520px";
+        setAction("Login");
+    }
+
+    const register = () => {
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        x.style.left = "-510px";
+        y.style.right = "5px";
+        setAction("Register");
+    }
+
+    useEffect(() => {
+        document.getElementById("loginBtn").addEventListener("click", login);
+        document.getElementById("registerBtn").addEventListener("click", register);
+
+        return () => {
+            document.getElementById("loginBtn").removeEventListener("click", login);
+            document.getElementById("registerBtn").removeEventListener("click", register);
+        };
+    }, []);
+
+    return (
+        <div id="wrapper">
+            <nav className="nav">
+                <div className="nav-logo">
+                    <p>Leave Management</p>
+                </div>
+                <div className="nav-button">
+                    <button className={`btn ${action === "Login" ? "white-btn" : ""}`} id="loginBtn">Sign In</button>
+                    <button className={`btn ${action === "Register" ? "white-btn" : ""}`} id="registerBtn">Sign Up</button>
+                </div>
+                <div className="nav-menu-btn">
+                    <i className="bx bx-menu" onClick={myMenuFunction}></i>
+                </div>
+            </nav>
+
+            <div className="form-box">
+                <div className="login-container" id="login">
+                    <div className="top">
+                        <span>Don't have an account? <a href="#" onClick={() => register()}>Sign Up</a></span>
+                        <header>Login</header>
+                    </div>
+                    <div className="input-box">
+                        <input type="text" className="input-field" placeholder="Username or Email" />
+                        <i className="bx bx-user"></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="password" className="input-field" placeholder="Password" />
+                        <i className="bx bx-lock-alt"></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="submit" className="submit" value="Sign In" />
+                    </div>
+                    <div className="two-col">
+                        <div className="one">
+                            <input type="checkbox" id="login-check" />
+                            <label htmlFor="login-check"> Remember Me</label>
+                        </div>
+                        <div className="two">
+                            <label><a href="#">Forgot password?</a></label>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="register-container" id="register">
+                    <div className="top">
+                        <span>Have an account? <a href="#" onClick={() => login()}>Login</a></span>
+                        <header>Sign Up</header>
+                    </div>
+                    <div className="two-forms">
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Firstname" />
+                            <i className="bx bx-user"></i>
+                        </div>
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Lastname" />
+                            <i className="bx bx-user"></i>
+                        </div>
+                    </div>
+                    <div className="input-box">
+                        <input type="text" className="input-field" placeholder="Email" />
+                        <i className="bx bx-envelope"></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="password" className="input-field" placeholder="Password" />
+                        <i className="bx bx-lock-alt"></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="submit" className="submit" value="Register" />
+                    </div>
+                    <div className="two-col">
+                        <div className="one">
+                            <input type="checkbox" id="register-check" />
+                            <label htmlFor="register-check"> Remember Me</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginSignup;
